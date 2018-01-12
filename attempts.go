89f759ddbe92576291ec6d5ab1a9ac2f27fa548c
Kane Lewis <C-Nicholas.Lewis@charter.com>
2018-01-12 12:54:42 -0700

    3

--- attempts.go
+++ attempts.go
@@ -8,10 +8,18 @@ package main
         "os"
     )
 
+    func Unmarshal(data []byte, v interface{}) error
 
+    func check(e error) {
+        if e != nil {
+            panic(e)
+        }
+    }
 
+    func main() {
 
-
-    datafile, err := ioutil.ReadFile("payconfig.dat")
+    var datafile, err := ioutil.ReadFile("payconfig.dat")
     check(err)
     fmt.Print(string(datafile))
+
+    }
