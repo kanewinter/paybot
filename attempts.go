b3c55b1940e5d874cfda71eaa68dd5137273b555
Kane Lewis <C-Nicholas.Lewis@charter.com>
2018-01-12 13:48:47 -0700

    11

--- attempts.go
+++ attempts.go
@@ -19,14 +19,18 @@ package main
     datafile, err := ioutil.ReadFile("payconfig.dat")
     check(err)
     fmt.Print(string(datafile))
+    fmt.Println()
 
     var jsondata interface{}
     json.Unmarshal(datafile, &jsondata)
     fmt.Print(interface{}(jsondata))
+    fmt.Println()
 
-    m := jsondata.(map[string]interface{})
+    m := jsondata.customerdata
     fmt.Println(m)
+
+    fmt.Println()
     //payee := m.[]interface{}
-    //fmt.Println(payee)
+    //fmt.Println(payee\n)
 
     }
