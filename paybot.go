bcd9fb06f41c59a234975f3de570b583b4abb23d
Kane <kane@kane.ventures>
2018-02-12 19:28:41 -0700

    2

--- paybot.go
+++ paybot.go
@@ -121,7 +121,7 @@ package main
         	log.Fatal(err)
         }
        	//result.WriteString(out.String())
-        fmt.Println(out)
+        fmt.Println(out.String)
         var tmp bytes.Buffer
         tmp.WriteString(out.String())
         var stuff string = tmp.String()
